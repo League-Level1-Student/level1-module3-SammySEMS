9da0d785efbe3c1eb3ad5746b59e9f1cde027bde
student <student@ip-172-31-61-185.us-west-2.compute.internal>
2021-10-25 18:27:53 -0700

    Smile

---
+++ src/_01_intro_to_static/Athlete.java
@@ -0,0 +1,17 @@
+package _01_intro_to_static;
+
+public class Athlete {
+	 static int nextBibNumber=1;
+	    static String raceLocation = "New York";
+	    static String raceStartTime = "9.00am";
+
+	    String name;
+	    int speed;
+	   public int bibNumber;
+	    public Athlete(String name, int speed){
+	        this.name = name;
+	        this.speed = speed;
+	        this.bibNumber=nextBibNumber;
+	        nextBibNumber++;
+	    }
+}
---
+++ src/_01_intro_to_static/AthleteMarathonRunner.java
@@ -0,0 +1,21 @@
+package _01_intro_to_static;
+
+public class AthleteMarathonRunner{
+	
+	public static void main(String[] args) {
+	
+	Athlete bob = new Athlete("Bob", 5);
+	Athlete mark = new Athlete("Mark", 35);
+    //create two athletes
+    //print their names, bibNumbers, and the location of their race. 
+	System.out.println(bob.name);
+	System.out.println(bob.speed);
+	
+	System.out.println(mark.name);
+	System.out.println(mark.speed);
+	System.out.println("New York");
+	System.out.println("9:00 AM");
+	
+	System.out.println(bibNumber);
+	
+}}
